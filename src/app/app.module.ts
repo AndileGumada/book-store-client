4e108e1aea440bef0868acd68ef221e571973c6f
AndileGumada <andile.gumada@jumpco.io>
2020-05-28 11:11:24 +0200

    Angular client application for the online book-store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { BookService } from './sevices/book.service';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
+
+const routes : Routes =[
+
+  {path : 'books', component : BookListComponent},
+  {path : 'category/:id', component : BookListComponent},
+  {path : '' ,redirectTo: '/books' ,pathMatch: 'full' },
+  {path : '**', component : PageNotFoundComponent}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    RouterModule.forRoot(routes )
   ],
-  providers: [],
+  providers: [
+    BookService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/sevices/book.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { Book } from '../common/book';
+import { map } from 'rxjs/operators';
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+  private baseUrl = "http://localhost:4000/books";
+
+  constructor(private httpClient : HttpClient) { }
+    
+  
+getBooks(theCategoryId : number): Observable<Book[]>{
+    const searchUrl = `${this.baseUrl}/search/categoryid?id=${theCategoryId}`;
+      
+      return this.httpClient.get<GetResponseBooks>(searchUrl).pipe(
+        map( response => response._embedded.books )
+      );
+    }
+}
+interface GetResponseBooks{
+   _embedded: {
+     books : Book[];
+   }
+ }
